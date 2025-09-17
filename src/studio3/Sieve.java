212ee23a0afed124907e14bb37bbbb5048e97f6a
Avi Narepalem <a.d.narepalem@wustl.edu>
2025-09-17 15:46:22 -0500

    hi

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Sieve {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(in.nextInt());
+
+        boolean [] prime = new boolean[n-2];
+        for (int i = 0; n < prime.length; i++);{
+            
+        }
+
+         
+       
